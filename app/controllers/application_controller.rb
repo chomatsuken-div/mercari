db7362b993fb8e37f268953edd62e69f51cb5442
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2019-04-02 17:59:34 +0900

    Deproy7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,8 @@ class ApplicationController < ActionController::Base
   end
 
   def basic_auth
-    user = ENV['BASIC_AUTH_USER']
-    pass = ENV['BASIC_AUTH_PASSWORD']
     authenticate_or_request_with_http_basic do |username, password|
-      username == user && password == pass
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
