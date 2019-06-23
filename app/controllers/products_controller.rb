b6e3cc7d7cc59c4812dc53a9afd751b8dc20a08f
chomatsuken-div <shogo.chomatsuken@di-v.co.jp>
2019-06-23 19:19:31 +0900

    error_handling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,8 +36,11 @@ class ProductsController < ApplicationController
 
   def destroy
     product = Product.find(params[:id])
-    product.destroy
-    redirect_to root_path
+    if product.destroy
+      redirect_to root_path
+    else
+      redirect_to product_path
+    end
   end
 
   private
